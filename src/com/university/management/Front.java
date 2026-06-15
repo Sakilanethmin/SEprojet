3cc4ef18642c7a400cd38faf4d24d737437a1bd4
lisarahewawasam <lisarahewawasam06@gmail.com>
2026-06-15 19:24:44 +0530

    fix: replace menu header output with logger

--- src/com/university/management/Front.java
+++ src/com/university/management/Front.java
@@ -1,14 +1,16 @@
 package com.university.management;
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class Front {
+    private static final Logger LOGGER = Logger.getLogger(Front.class.getName());
     static Scanner input = new Scanner(System.in);
 
     private Front() {
     }
 
     public static int menu(){
-        System.out.println("-------------------------------------Menu------------------------------------------------");
+        LOGGER.info("-------------------------------------Menu------------------------------------------------");
         System.out.println("Choose option: ");
         System.out.println("1. Attendance");
         System.out.println("2. Login");
