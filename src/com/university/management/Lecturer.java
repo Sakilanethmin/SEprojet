502ca51bd46b3fcc20864426e14b318de268facc
Rajitha Rajakaruna <viraj24.rajakaruna@gmail.com>
2026-06-15 19:32:58 +0530

    Fix : replaced System.out.println with a Logger Fix : Added @Override annotation Fix : use {0} placeholder replacing +

--- src/com/university/management/Lecturer.java
+++ src/com/university/management/Lecturer.java
@@ -1,5 +1,8 @@
 package com.university.management;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 public class Lecturer extends Person implements CourseViewable {
+    private static final Logger logger = Logger.getLogger(Lecturer.class.getName());
     Course[] addCourses = new Course[10];
     private int i = 0;
     protected String department;
@@ -12,19 +15,22 @@ public class Lecturer extends Person implements CourseViewable {
        if (i < addCourses.length) {
            addCourses[i] = new Course(courseCode, courseName);
            i++;
-           System.out.println("Course added successfully");
+           logger.info("Course added successfully");
        }
        else{
-           System.out.println("Maximum course limit reached");
+           logger.info("Maximum course limit reached");
        }
    }
+   @Override
     public void displayInfo() {
         super.displayInfo();
-        System.out.println("Department: " +department);
+       logger.log(Level.INFO, "Department: {0}", department);
     }
+
+    @Override
    public void viewCourses() {
         if (i == 0){
-            System.out.println("No course added");
+            logger.info("No course added");
         }
         else {
             for(int j = 0; j < i; j++) {
